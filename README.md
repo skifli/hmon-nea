ca5b15fc44bfc74a1c97a82b7460b76aa0154e8b
skifli <121291719+skifli@users.noreply.github.com>
2026-07-30 17:46:41 +0100

    feat(README.md): update!

--- README.md
+++ README.md
@@ -1,3 +1,108 @@
 # hmon-nea
 
-This is the repository for the LaTeX write-up for my OCR A-Level Computer Science (H446) NEA. The repository for the code can be found here: https://github.com/exeme-project/hmon.
+This is the repository for the LaTeX write-up for my OCR A-Level Computer (H446)
+NEA. The code repository can be found
+[@exeme-project/hmon](https://github.com/exeme-project/hmon).
+
+> [!NOTE] The code repository is under
+> [exeme-project](https://github.com/exeme-project) as that is the organisation
+> I tend to use for some of my more ambitiuous programs, like my
+> [own programming language's compiler](https://github.com/exeme-project/exeme-lang)
+> (also written in C)!
+
+## Notes on working environment
+
+The repository is pre-configured to my liking to make it extremely easy for me
+to get started on work with the NEA whenever I want to, but that does mean if
+you aren't in the same environment as I have setup stuff can easily break. If
+you're planning to replicate this environment you should probably read the
+following:
+
+### General
+
+1. My OS is [nixOS](https://nixos.org/), which is important as it's
+   declarative - i.e., I have code telling the OS not just what apps to install
+   but how to configure them. The repository for my nixOS configuration can be
+   found [@skifli/nixos](https://github.com/skifli/nixos).
+2. I use [Zed](https://zed.dev/) as my editor of choice for the NEA - although
+   when I'm doing some terminal work I do also use
+   [Helix](https://helix-editor.com/).
+
+- The first thing to note is that I have scripts bound to compositor keys that
+  automatically open all the windows I need for my NEA, and organise them onto
+  my two monitors - the specific script for my NEA is called
+  [2-niri.sh](https://github.com/skifli/nixos/blob/main/users/ami/scripts/2-niri.sh).
+- This means that whenever I want to do work on my NEA I can just
+  [press `Mod+Ctrl+2`](https://github.com/skifli/nixos/blob/2bf727b805cd40c07e6ee4ab36d4f43df8edaaf7/users/programs/compositor/niri/binds.nix#L345),
+  and it'll open my PDF viewer, editor, etc., and organise them on the monitors
+  and in the sizes I want.
+- However, most of that script probably won't be useful for anyone else, with
+  the exception of
+  [lines 12 to 20](https://github.com/skifli/nixos/blob/2bf727b805cd40c07e6ee4ab36d4f43df8edaaf7/users/ami/scripts/2-niri.sh#L12).
+  Those lines launch (if it isn't already running)
+  [src/zeditor-synctex.sh](src/zeditor-synctex.sh) - more information on SyncTeX
+  later, but since that's not included in this repository setup you should
+  probably work out some other way of starting that script whenever you open
+  this directory.
+
+> [!NOTE] These scripts also require
+> [jq](https://github.com/skifli/nixos/commit/f4797f8ef6f3d425bd14e8d208f4454ae9efadaf#diff-7ca3f7dce1c0f19e09b9826d401954d02f09deee1125502809b89dfbded43149R190)
+> to be installed on your system if it's not already.
+
+### devenv
+
+As I'm already using Nix I use [devenv](https://devenv.sh/) to manage my
+environment - it's a bit like virtualenv, but manages not just Python packages
+but everything. The devenv configuration is in [flake.nix](flake.nix), and
+contains all the packages needed for my environment.
+
+To use my development environment (on nixOS), clone this repository, open in
+your terminal, and type `direnv allow`. From that moment on whenever you open
+the cloned repository directory, you will be placed in a environment managed by
+direnv that contains all of the packages needed for writing the NEA.
+
+### Zed setup
+
+As I use Zed for editing, there is a settings configuration file in
+[.zed/settings.json](.zed/settings.json) that should automatically configure
+markdown to be formatted using `prettier` (again make sure it's installed on
+your system), and also configures SyncTeX forward searching (see the below
+section for more information). Don't forget to also install the `markdownlint`
+extension though as well.
+
+### SyncTeX
+
+This connects the source code to the output PDF. Essentially, if configured
+properly it means that whenever the LaTeX is updated the PDF viewer auto scrolls
+to the updated area (if it's not already in the viewport) and highlights the
+change, and also if you Ctrl Click on an area of the PDF it goes to the exact
+line of code that defines it in the relevant LaTeX file.
+
+I have it fully setup, but it relies on the following assumptions - change if
+they are different for you:
+
+- I use the [Evince](https://wiki.gnome.org/Apps/Evince) PDF viewer. If you
+  don't, it's going to be a bit harder to change as you need to find a specific
+  program that allows that PDF viewer to interface with SyncTex. The
+  [rzukic/zed-latex wiki](https://github.com/rzukic/zed-latex/wiki/Preview)
+  might be a good place to start. You need to first change the
+  [flake.nix](flake.nix) file to package your PDF viewer's SyncTex interface
+  program however relevant. Then, you need to update
+  [.zed/settings.json](.zed/settings.json) under `forwardSearch` to run whatever
+  new binary you have installed, with whatever arguments it expects. Lastly, you
+  need to update [src/zeditor-synctex.sh](src/zeditor-synctex.sh) with code to
+  communicate with your chosen PDF viewer. This should allow forward searching,
+  and backwards searching, respectively.
+
+- I use the [Zed](https://zed.dev) editor. The `latex` extension automatically
+  builds the output PDF on any change, so if your editor doesn't do that you
+  should work out how to do so. As I also use Evince as aforementioned, it
+  automatically reloads the PDF on change (and remembers state). Lastly, I have
+  Zed configured to automatically perform forward searches for Evince in
+  [.zed/settings.json](.zed/settings.json) - again this should be setup in your
+  editor of choice.
+- Whenever I start working on my NEA my computer automatically runs the
+  [src/zeditor-synctex.sh](src/zeditor-synctex.sh) script - as aforementioned
+  this is baked into my hotkey setup, but you should also look into starting
+  this automatically - e.g., maybe with direnv? I dunno! What this allows though
+  is the backwards searching functionality of SyncTeX.
--- src/zeditor-synctex.sh
+++ src/zeditor-synctex.sh
@@ -1,181 +1,48 @@
 #!/bin/bash
 # Based on https://github.com/Vinno97/evince-synctex/blob/master/evince-synctex.sh
-# Sync LaTeX editor with Evince
+# Sync LaTeX editor with Evince (Backward Sync Only)
 # shellcheck disable=2206
 
 set -e
-# set -x
-
-usage="Usage:  evince-synctex.sh <method> [options...]
-        evince-synctex.sh listen [command] [args...]
-        evince-synctex.sh sync <pdfpath> <texpath> <line>
-        evince-synctex.sh --help"
-
-help="Sync your editor with Evince - the GNOME Document Viewer
-
-Uses the D-Bus to send or listen to Evince's sync requests for forwards or
-backwards sync. Allows arbitrary editors to sync editors with Evince.
-
-Requires the PDF to be compiled using LaTeX with 'synctex=1'.
-
-$usage
-
-Methods:
-    sync            Performs a forward sync.
-                    Attempts to sync an open Evince window to a specific line
-                    of a TeX file. Only works when the PDF is already opened.
-    listen          Listens for a backward sync.
-                    Waits for Evince's 'SyncSource' signal on the D-Bus and
-                    executes a command when one is received.
-                    Defaults to running 'code --goto %f:%l'
-
-Options:
-    Sync
-        pdfpath     The path to the target PDF file
-        texpath     The path to the originating TeX file
-        line        The line of the TeX file to sync to
-    Listen
-        command     Command that is ran when Evince requests a backwards sync
-        args...     Arguments to the commands, uses the following replacements:
-                        %f  Gets replaced by the full TeX filename
-                        %l  Gets replaced by the line number
-
-Examples:
-    Forward Sync:
-        evince-synctex.sh sync document.pdf document.tex 30
-
-    Backward Sync using VS Code (default):
-        evince-synctex.sh listen code --goto %f:%l
-    Backward Sync using Sublime:
-        evince-synctex.sh listen subl %f:%l
-    Backward Sync using Gedit (only works when file is already open):
-        evince-synctex.sh listen gedit %f +%l
-"
 
 urldecode() {
-    # https://stackoverflow.com/a/37840948
     local url=${*//+/ }     # replace "+" with " " (space)
     echo -e "${url//%/\\x}" # replace "%" with "\x" and do hex expansion
 }
 
-sync() {
-    if [ ! $# -eq 3 ]; then
-        echo -e "Expected exactly 3 arguments for sync, got $#\n$usage" 1>&2
-        exit 1
+cmd=${*:-'zeditor %f:%l'}
+echo "Listening to Evince SyncSource requests. Executing '$cmd' on new signals"
+
+dbus-monitor "type=signal,interface=org.gnome.evince.Window,member=SyncSource" |
+while read -r line
+do
+    parts=($line)
+
+    if [ "${parts[0]}" == signal ]; then
+        exc_cmd=($cmd)
+        filename=""
+        linenr=""
+    elif [ "${parts[0]}" == string ]; then
+        filename=${parts[1]}
+    elif [ "${parts[0]}" == int32 ] && [ -z "$linenr" ]; then
+        linenr=${parts[1]}
+
+        echo ""
+
+        filename=$(urldecode "$filename")
+        filename=${filename/#\"file:\/\//} # strips the leading '"file://'
+        filename=${filename/%\"/}          # strips the trailing '"'
+        echo "Filename: $filename"
+        echo "Line number: $linenr"
+
+        for i in "${!exc_cmd[@]}"; do
+            # Do all replacements here
+            exc_cmd[i]=${exc_cmd[$i]//%l/"$linenr"}
+            exc_cmd[i]=${exc_cmd[$i]//%f/"$filename"}
+        done
+
+        echo "Executing: '${exc_cmd[*]}'"
+
+        "${exc_cmd[@]}" < /dev/null
     fi
-
-    pdfpath=$(readlink -f "$1")
-    srcpath=$(readlink -f "$2")
-    line=$3
-
-    if ! [ -f "$srcpath" ]; then
-        echo "Warning: $srcpath does not exists." 1>&2
-    fi
-    if ! [ -f "$pdfpath" ]; then
-        echo "Warning: $pdfpath does not exists." 1>&2
-    fi
-    if ! [ "$line" -eq "$line" ] 2>/dev/null; then
-        echo -e "<line> should be a number, got $line\n$usage" 1>&2
-        exit 1
-    fi
-
-    echo "Syncing Evince to line $line of '$(basename "$srcpath")' for '$(basename "$pdfpath")'"
-
-    local string="$pdfpath"
-    local length="${#string}"
-    local encoded=""
-    local i c
-
-    for (( i = 0; i < length; i++ )); do
-        c="${string:i:1}"
-        case "$c" in
-            [a-zA-Z0-9.~_/-]) encoded+="$c" ;;
-            *) printf -v hex '%%%02X' "'$c"; encoded+="$hex" ;;
-        esac
-    done
-
-    pdfuri="file://$encoded"
-
-    destination=$(gdbus call \
-    --session \
-    -d org.gnome.evince.Daemon \
-    -o /org/gnome/evince/Daemon \
-    -m org.gnome.evince.Daemon.FindDocument \
-    "$pdfuri" false | cut -d\' -f2)
-
-    if [ -z "$destination" ]; then
-        echo "No Evince window found for $pdfpath" 1>&2
-        exit 1
-    fi
-
-    gdbus call \
-    --session \
-    -d "$destination" \
-    -o /org/gnome/evince/Window/0 \
-    -m org.gnome.evince.Window.SyncView \
-    "$srcpath" "($line, 1)" 0 > /dev/null
-
-    echo "Done!"
-
-}
-
-listen() {
-    cmd=${*:-'zeditor %f:%l'}
-    echo "Listening to Evince SyncSource requests. Executing '$cmd' on new signals"
-    # echo "Running command '$cmd' on SyncSource request"
-
-
-    dbus-monitor "type=signal,interface=org.gnome.evince.Window,member=SyncSource" |
-    while read -r line
-    do
-        parts=($line)
-
-        if [ "${parts[0]}" == signal ]; then
-            exc_cmd=($cmd)
-            filename=""
-            linenr=""
-        elif [ "${parts[0]}" == string ]; then
-            filename=${parts[1]}
-        elif [ "${parts[0]}" == int32 ] && [ -z "$linenr" ]; then
-            linenr=${parts[1]}
-
-            echo "=================================================="
-
-            filename=$(urldecode "$filename")
-            filename=${filename/#\"file:\/\//} # strips the leading '"file://'
-            filename=${filename/%\"/}          # strips the trailing '"'
-            echo "Filename: $filename"
-            echo "Line number: $linenr"
-
-            for i in "${!exc_cmd[@]}"; do
-                # Do all replacements here
-                exc_cmd[i]=${exc_cmd[$i]//%l/"$linenr"}
-                exc_cmd[i]=${exc_cmd[$i]//%f/"$filename"}
-            done
-
-            echo "Executing: '${exc_cmd[*]}'"
-
-            "${exc_cmd[@]}" < /dev/null
-        fi
-    done
-}
-
-
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
-    echo -e "$help"
-    exit 0
-fi
-
-if [ $# -eq 0 ]; then
-    echo -e "$usage" 1>&2
-    exit 1
-fi
-
-method="$1"
-shift 1
-
-case $method in
-  sync) sync "$@";;
-  listen) listen "$@";;
-  * ) echo -e "Unknown method \"$method\", expected one of: sync|listen\n$usage" 1>&2; exit 1;;
-esac
+done
